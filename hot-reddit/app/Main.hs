eb6dbd9cca4f12d65260697692a4f90b1d687f01
ruct <flyce32@yandex.ru>
2022-12-25 21:10:49 +0300

    Add lib function

--- hot-reddit/app/Main.hs
+++ hot-reddit/app/Main.hs
@@ -2,21 +2,12 @@
 module Main (main) where
 
 import Lib
-
-import qualified Network.HTTP.Client     as H
-import qualified Network.HTTP.Client.TLS as H
-import Network.HTTP.Client.OpenSSL
-import OpenSSL.Session (context)
-import Network.Wreq
-import Control.Lens
-
+import System.Environment
+import Data.ByteString.UTF8 as BSU
 
 main :: IO ()
 main = do
-  let opts = defaults & manager .~ Left (opensslManagerSettings context)
-                      & header "Authorisation" .~ ["AUTH_INFO_STRING"] 
-  r <- withOpenSSL $
-    getWith opts "https://httpbin.org/get"
-  print r
-  print "kek"
+  token <- getEnv "REDDIT_ACCESS_TOKEN"
+  kek <- redditHotApiRequest (BSU.fromString token)
+  print kek
 
--- hot-reddit/src/Lib.hs
+++ hot-reddit/src/Lib.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( redditHotApiRequest
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Network.HTTP.Client.OpenSSL
+import OpenSSL.Session (context)
+-- import Data.ByteString.UTF8 as BSU
+import Data.ByteString.Lazy.UTF8 as BLU
+import Data.ByteString as DB
+import Network.Wreq
+import Control.Lens
+
+redditHotApiRequest :: DB.ByteString -> IO BLU.ByteString
+redditHotApiRequest token = do
+  let opts = defaults & manager .~ Left (opensslManagerSettings context)
+                      & header "Authorization" .~ [DB.concat ["bearer", token]]
+                      & header "User-Agent" .~ ["APP-NAME by REDDIT-USERNAME"]
+  r <- withOpenSSL $
+    getWith opts "https://oauth.reddit.com/hot"
+  return $ r ^. responseBody
